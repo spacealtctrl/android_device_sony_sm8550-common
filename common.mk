a3987a1626c631968d7139d4d0e6e9a026a356db
ˢᶜᵒᵗᵗඞ⁩🛸 <scott@dbzmail.com>
2025-12-03 10:45:35 +0000

    Update Dolby Atmos product inheritance path

--- common.mk
+++ common.mk
@@ -476,7 +476,7 @@ PRODUCT_PACKAGES += \
     XperiaParts
 
 # Dolby Atmos
-$(call inherit-product, vendor/dolby/dolby.mk)
+$(call inherit-product, hardware/dolby/dolby.mk)
 
 # Sony Camera
 $(call inherit-product, vendor/sony/extra/camera.mk)
