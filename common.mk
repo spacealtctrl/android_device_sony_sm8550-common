f8b52eaccb81419ad0a1232d2bbbfa62f5682169
ˢᶜᵒᵗᵗඞ⁩🛸 <scott@dbzmail.com>
2026-01-07 04:15:30 +0000

    chore: verified namespace config against tyty reference

--- common.mk
+++ common.mk
@@ -350,7 +350,8 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/sony
+    hardware/sony \
+    hardware/qcom-caf/sm8550
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
