4a07521aeb4b8ee6232fafdebad104fb431d3987
ˢᶜᵒᵗᵗඞ⁩🛸 <scott@dbzmail.com>
2026-01-07 03:30:56 +0000

    fix: sync audio config

--- common.mk
+++ common.mk
@@ -476,7 +476,7 @@ PRODUCT_PACKAGES += \
     XperiaParts
 
 # Dolby
-$(call inherit-product, vendor/dolby/audio.mk)
+$(call inherit-product-if-exists, vendor/sony/dolby/audio.mk)
 
 # Sony Extra Apps
 $(call inherit-product-if-exists, vendor/sony/extra/extra.mk)
