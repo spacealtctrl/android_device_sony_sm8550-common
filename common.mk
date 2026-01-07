e12054210fdc42358f63a09ac00ea31a2944b8f3
ˢᶜᵒᵗᵗඞ⁩🛸 <scott@dbzmail.com>
2026-01-07 05:35:36 +0000

    Update product inheritance in common.mk

--- common.mk
+++ common.mk
@@ -477,10 +477,10 @@ PRODUCT_PACKAGES += \
     XperiaParts
 
 # Dolby
-$(call inherit-product-if-exists, vendor/sony/dolby/audio.mk)
+$(call inherit-product, vendor/dolby/audio.mk)
 
 # Sony Extra Apps
-$(call inherit-product-if-exists, vendor/sony/extra/extra.mk)
+$(call inherit-product, vendor/sony/extra/extra.mk)
 
 # Inherit from proprietary files makefile
 $(call inherit-product, vendor/sony/sm8550-common/sm8550-common-vendor.mk)
