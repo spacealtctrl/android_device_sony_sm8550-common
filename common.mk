6f0f62ebe8bbb67c40a567c26cf1f2c36a688697
ˢᶜᵒᵗᵗඞ⁩🛸 <scott@dbzmail.com>
2026-01-08 10:10:15 +0000

    Remove soundtrigger implementation from common.mk

--- common.mk
+++ common.mk
@@ -35,7 +35,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
