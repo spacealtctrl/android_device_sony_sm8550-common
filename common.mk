9897c070827241bb82d0cb89f9d08c4e9ded4740
ˢᶜᵒᵗᵗඞ⁩🛸 <scott@dbzmail.com>
2026-01-08 09:22:44 +0000

    fix: remove stock audio apps from build configuration to ensure SonyMusicFX functions independently

--- common.mk
+++ common.mk
@@ -483,5 +483,3 @@ $(call inherit-product-if-exists, vendor/sony/extra/extra.mk)
 # Inherit from proprietary files makefile
 $(call inherit-product, vendor/sony/sm8550-common/sm8550-common-vendor.mk)
 
-# KILL LIST: Remove stock audio apps so SonyMusicFX stands alone
-PRODUCT_PACKAGES := $(filter-out AudioFX MusicFX com.android.musicfx org.lineageos.audiofx,$(PRODUCT_PACKAGES))
